b0026f7e0d810ce8b38ea676a5cf7d2363e60aff
MitsukoK <mitsukokichida1@gmail.com>
2022-10-30 19:58:09 +0600

    trusted origin

--- tekapakhi/settings.py
+++ tekapakhi/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+CSRF_TRUSTED_ORIGINS = ["https://takapakhi.herokuapp.com"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [

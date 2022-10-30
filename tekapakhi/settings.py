717a3616fe2d0d835ee4075291cf143a906e36c8
MitsukoK <mitsukokichida1@gmail.com>
2022-10-30 18:41:00 +0600

    removed shitty vercel and added heruko

--- tekapakhi/settings.py
+++ tekapakhi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-8c1%w48pbtbb($fz^c+a16_ds$v-1ud@zl63cfn-qd((=pkl)m
 DEBUG = True
 
 # ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
